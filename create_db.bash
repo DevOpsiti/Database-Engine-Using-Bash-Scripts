060d994b553d6f689ceeef9866225bf944f9e8d7
DevOpsiti <111522901+DevOpsiti@users.noreply.github.com>
2022-09-02 09:18:34 +0200

    Add files via upload

--- create_db.bash
+++ create_db.bash
@@ -1,55 +1,89 @@
 #!/usr/bin/bash
-
 ## Test Case:
-
-
-
-echo "********************************* Create New Database *****************************"
-
+clear
+echo "********************************************************* Create New Database *********************************************"
 # input Database
-read -p "Your Database Name: " d_n
+read -p "Please Enter Your Database Name :" d_n
 
 # check database if exist
 if [[ -d $d_n ]];
 then
-        echo "Opps, Database name is already exist !!"
-        select te in ceate_another_database back_menu 
+        echo "Opps, This Database Is Already Existed !!"
+        select te in connect_to_database add_another_database back_menu 
         do
         case $te in
-        ceate_another_database ) . create_db.bash;;
+        connect_to_database ) . connect_db.bash;;
+        add_another_database ) . create_db.bash;;
         back_menu ) . main.bash;;
         * ) echo "Invaled Number" ;;
         esac
         done
 
-#check input include data or empty
+# if input is empty
 elif [[ -z $d_n ]];
 then
-        echo "Opps, Please enter your database name ?!"
-        select te in enter_the_database_name_again back_menu 
+        echo "Opps, Please Enter Your Database Name ?!!"
+        select te in try_again back_menu 
         do
         case $te in
-        enter_the_database_name_again ) . create_db.bash;;
+        try_again ) . create_db.bash;;
         back_menu ) . main.bash;;
         * ) echo "Invaled Number" ;;
         esac
         done
 
-# check database if not exist
+# if database is not exist
 elif [[ ! -d $d_n ]] ;
 then
-mkdir $d_n
-echo "The Database has been created successfully"
-select te in add_another_database back_menu 
-do
-case $te in
-add_another_database ) . create_db.bash ;;
-back_menu ) . main.bash;;
- * ) echo "Invaled Number" ;;
-esac
-done
+        # If you find any spaces in the input
+        if [[ $d_n == *" "* ]]
+        then
+                x=$(echo $d_n | sed 's/ /_/g')
+                mkdir $x
+                echo "$x Created Sucessfully"
+                select te in add_another_database back_menu 
+                do
+                case $te in
+                add_another_database ) . create_db.bash ;;
+                back_menu ) . main.bash;;
+                * ) echo "Invaled Number" ;;
+                esac
+                done
+        elif [[ $d_n == [0-9]* ]];
+        then
+                echo "Opps, There is a syntax error"
+                echo "It is not allowed to enter numbers at the beginning"
+                        select te in try_again back_menu 
+                        do
+                        case $te in
+                        try_again ) . create_db.bash;;
+                        back_menu ) . main.bash;;
+                        * ) echo "Invaled Number" ;;
+                        esac
+                        done
+        elif [[ $d_n == [a-z]* ]]
+        then
+                echo "Opps, There is a syntax error"
+                echo "It is not allowed to enter a lowercase letter at the beginning"
+                        select te in try_again back_menu 
+                        do
+                        case $te in
+                        try_again ) . create_db.bash;;
+                        back_menu ) . main.bash;;
+                        * ) echo "Invaled Number" ;;
+                        esac
+                        done
+        else 
+
+                mkdir $d_n
+                echo "$d_n Created Sucessfully"
+                select te in add_another_database back_menu 
+                do
+                case $te in
+                add_another_database ) . create_db.bash ;;
+                back_menu ) . main.bash;;
+                * ) echo "Invaled Number" ;;
+                esac
+                done
+        fi
 fi
-
-
-
-
